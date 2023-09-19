59bd718b6e87a34dad9bbc1af91638c8b916d043
viniciusrvcruz <viniciusrobertopro@gmail.com>
2023-09-19 16:47:07 -0300

    mudei as rotas

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -7,6 +7,6 @@ export const PageNotFound = () => {
     margin: '20px 20px 500px',
   };
   return (
-    <div style={divStyle}>Pagina não encontrada Volte para o <Link to="/gerenciador-de-contratos">home</Link></div>
+    <div style={divStyle}>Pagina não encontrada Volte para o <Link to="/">home</Link></div>
   )
 }
